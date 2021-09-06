10a1641fd845cd5e9321e15a7c845d8cb9cb7f78
My <myle10032001@gmail.com>
2021-09-06 09:44:28 +0700

    lesson02-teemy 06.09.2021

---
+++ lesson02-teemy/Lesson02_teemy/src/Ex02.java
@@ -0,0 +1,17 @@
+
+public class Ex02 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 1;
+		int s = 0;
+		while(i<=18 ) {
+			s+=i;
+			i++;
+			if(i==4) sum+=s;
+			if(i==7) sum+=s;
+			if(i==12) sum+=s;
+			if(i==18) sum+=s;
+		}
+		System.out.println("tong = "+sum);
+	}
+}
