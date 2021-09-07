64dd01f389b8e9c5dac997dfb9c841181fd37239
MGM\tcho <trung.cong.ho@mgm-tp.com>
2021-09-07 19:58:38 +0700

    trungho exercie 7

--- lesson02-trungho/src/TrungHoHomeWork/Ex07.java
+++ lesson02-trungho/src/TrungHoHomeWork/Ex07.java
@@ -1,7 +1,43 @@
 package TrungHoHomeWork;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex07 {
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		Scanner ip = new Scanner(System.in);
 		
+		System.out.println("Please enter min random value: ");
+		int min = Integer.parseInt(ip.nextLine());
+		
+		System.out.println("Please enter max random value: ");
+		int max = Integer.parseInt(ip.nextLine());
+		
+		System.out.println("Please enter amount of random value you want: ");
+		int numberOfElement = Integer.parseInt(ip.nextLine());
+		
+		System.out.println("Random result: " + randomExhaustedInteger(min, max, numberOfElement));
+	}
+	
+	public static ArrayList<Integer> randomExhaustedInteger(int min, int max, int numberOfElement) {		
+		while(numberOfElement > (max - min + 1)) {
+			System.out.println("Please re-enter the number of value, it should be equal or less than: " + (max - min + 1));
+			Scanner ip = new Scanner(System.in);
+			numberOfElement = Integer.parseInt(ip.nextLine());
+		}
+		
+		ArrayList<Integer> randomResult = new ArrayList<>();
+		Random rd = new Random();
+		
+		while (randomResult.size() < numberOfElement) {
+			int random = rd.nextInt((max-min) + 1) + min;
+			
+			if(!randomResult.contains(random)) {
+				randomResult.add(random);
+			}
+		}
+		
+		return randomResult;
 	}
 }
