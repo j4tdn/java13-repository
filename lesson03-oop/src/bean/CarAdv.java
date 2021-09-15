5bdc6672d1e306bce3ca7ffdbcf3c3548b9eaeb4
nuhigin <nuhigin@gmail.com>
2021-09-15 23:14:53 +0700

    lesson03 15.09.21

---
+++ lesson03-oop/src/bean/CarAdv.java
@@ -0,0 +1,33 @@
+package bean;
+
+public class CarAdv {
+	
+	// static >> initial value when class is being loaded
+	// non-static >> initial value when initial an object from class
+	
+	// static things will be created before non-static things
+	public static String model;
+
+	private String color;
+	
+	public CarAdv() {
+	}
+
+	public CarAdv(String color) {
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return model + ", " + color;
+	}
+	
+}
