9f86919606021008dc5446c10751db767f25f395
Duyên Trần Thị Mỹ <102190110@sv1.dut.udn.vn>
2021-09-15 23:12:48 +0700

    lesson03-oop 15.09.2021

---
+++ lesson03-oop/src/view/Ex01.java
@@ -0,0 +1,16 @@
+package view;
+
+import bean.Rectangle;
+
+public class Ex01 {
+ public static void main(String[] args) 
+ {
+	 Rectangle r1 = new Rectangle(0,4);
+	 r1.setWidth(8);
+	  System.out.println("r1" + r1);
+	 Rectangle r2 = new Rectangle(10,4);
+	 System.out.println("r2" + r2);
+	 Rectangle r3 = new Rectangle(3,7);
+	 System.out.println("r3" + r3);	
+ }
+}
