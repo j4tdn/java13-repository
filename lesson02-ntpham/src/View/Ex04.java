f1948d4a6fcac72cf0cc9deacc19c7e6713c7f60
ThanhNguyen1392001 <nguyen13701@gmail.com>
2021-09-15 23:17:30 +0700

    lesson3-oop 15.09.2021

---
+++ lesson02-ntpham/src/View/Ex04.java
@@ -0,0 +1,32 @@
+package View;
+
+import bean.CarAdv;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		// Tạo 100 mẫu xe Audi với các màu sắc khác nhau
+		// Audi >> Abba
+		
+		// Khi 1 thuộc tính, hàm mà không phụ thuộc vào đối tượng đang gọi
+		// >>>> Sử dụng static
+		
+		// Khi 1 thuộc tính, hàm mà phụ thuộc vào đối tượng đang gọi
+		// Hay là giá trị phụ thuộc vào đối tượng đang gọi
+		// >> Sử dụng non static
+		CarAdv.model = "Audi";
+		CarAdv c1 = new CarAdv("#fff");
+		CarAdv c2 = new CarAdv("#f00");
+		
+		CarAdv.model = "Abba";
+		CarAdv c3 = new CarAdv("#000");
+		CarAdv c4 = new CarAdv("#00f");
+		// ...
+		CarAdv c100 = new CarAdv("#ececec");	
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c100);
+	}
+}
