20f4a9a1531ddbd18898cb8531dd2358938cb668
Duyên Trần Thị Mỹ <102190110@sv1.dut.udn.vn>
2021-09-26 10:58:56 +0700

    lesson04-oop-features

--- design-patterns/src/factory/Shape.java
+++ design-patterns/src/factory/Shape.java
@@ -1,5 +1,10 @@
 package factory;
 
 public class Shape {
-
+	void drawing() {
+		System.out.println("shap>>drawing");
+	}
+	private void clear() {
+		return ;
+	}
 }
--- design-patterns/src/factory/ShapeFactory.java
+++ design-patterns/src/factory/ShapeFactory.java
@@ -1,5 +1,13 @@
 package factory;
 
 public class ShapeFactory {
-
+	private ShapeFactory() {
+		
+	}
+public static Shape createRectangle() {
+	return new Rectangle();
+}
+public static Shape createSquare() {
+	return new Square();
+}
 }
