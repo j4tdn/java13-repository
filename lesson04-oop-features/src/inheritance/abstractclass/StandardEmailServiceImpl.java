ae41beb6489200254804666015896a0fc07369ce
minh1902 <dovanmin1902@gmail.com>
2021-09-28 15:02:30 +0700

    lesson04-oop-features 28.9.2021

--- lesson04-oop-features/src/inheritance/abstractclass/StandardEmailServiceImpl.java
+++ lesson04-oop-features/src/inheritance/abstractclass/StandardEmailServiceImpl.java
@@ -1,5 +1,10 @@
 package inheritance.abstractclass;
 
-public class StandardEmailServiceImpl {
+public class StandardEmailServiceImpl extends EmailService{
 
+	@Override
+	void login() {
+		System.out.println("StandardEmailServiceImpl >> login ...");
+	}
+	
 }
