43aed7fe3532121090fd83968c48408655264d21
MGM\tcho <trung.cong.ho@mgm-tp.com>
2021-09-29 23:42:49 +0700

    lesson04-trungho Ex1

---
+++ lesson04-trungho/src/Ex01/Student.java
@@ -0,0 +1,59 @@
+package Ex01;
+
+public class Student {
+	private int id;
+	private String name;
+	private float pointLT;
+	private float pointTH;
+	
+	public Student() {
+	}
+
+	public Student(int id, String name, float pointLT, float pointTH) {
+		this.id = id;
+		this.name = name;
+		this.pointLT = pointLT;
+		this.pointTH = pointTH;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPointLT() {
+		return pointLT;
+	}
+
+	public void setPointLT(float pointLT) {
+		this.pointLT = pointLT;
+	}
+
+	public float getPointTH() {
+		return pointTH;
+	}
+
+	public void setPointTH(float pointTH) {
+		this.pointTH = pointTH;
+	}
+	
+	public float getAveragePoint() {
+		return (this.pointLT + this.pointTH) / 2;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", pointLT=" + pointLT + ", pointTH=" + pointTH + "]";
+	}
+}
---
+++ lesson04-trungho/src/Ex01/View.java
@@ -0,0 +1,70 @@
+package Ex01;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class View {
+	public static void main(String[] args) {
+		Student sv1 = new Student(1, "Dong", 9, 9);
+		Student sv2 = new Student(2, "Tay", 9, 8);
+		
+		Student sv3 = new Student();
+		enterStudentInfo(sv3);
+		
+		Student[] studentArray = {sv1, sv2, sv3};  
+		
+		getStudentWithAveragePointBiggerThan(studentArray, 8.5f);
+		System.out.println("=================================================");
+		getStudentWithPointLTBiggerThanPointTH(studentArray);
+	}
+	
+	public static void enterStudentInfo(Student sv) {
+		Scanner ip = new Scanner(System.in);
+		
+		System.out.println("Enter Student Id: ");
+		sv.setId(Integer.parseInt(ip.nextLine()));
+		
+		System.out.println("Enter Student Name: ");
+		sv.setName(ip.nextLine());
+		
+		System.out.println("Enter Student PointLT: ");
+		sv.setPointLT(Float.parseFloat(ip.nextLine()));
+		
+		System.out.println("Enter Student PointTH: ");
+		sv.setPointTH(Float.parseFloat(ip.nextLine()));
+	}
+	
+	public static ArrayList<Student> getStudentWithAveragePointBiggerThan(Student[] sv, float point) {
+		ArrayList<Student> studentResult = new ArrayList<Student>();
+		
+		for(int i = 0; i < sv.length; i++) {
+			if (sv[i].getAveragePoint() > point) {
+				studentResult.add(sv[i]);
+			}
+		}
+		
+		System.out.println("Students with average point > " + point + ": ");
+		studentResult.forEach(student ->{
+            System.out.println(student);
+        });
+		
+		return studentResult;
+	}
+	
+	public static ArrayList<Student> getStudentWithPointLTBiggerThanPointTH(Student[] sv) {
+		ArrayList<Student> studentResult = new ArrayList<Student>();
+		
+		for(int i = 0; i < sv.length; i++) {
+			if (sv[i].getPointLT() > sv[i].getPointTH()) {
+				studentResult.add(sv[i]);
+			}
+		}
+		
+		System.out.println("Students with PointLT Bigger Than PointTH: ");
+		studentResult.forEach(student ->{
+            System.out.println(student);
+        });
+		
+		return studentResult;
+	}
+}
