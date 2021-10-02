db20ea9a22f1efbd932c81de9543adc9d4a03171
pvkhai <vankhai12b@gmail.com>
2021-10-02 17:26:59 +0700

    lesson05-pvkhai 2.10.2021

---
+++ lesson05-pvkhai/src/ex05/Book.java
@@ -0,0 +1,43 @@
+package ex05;
+
+public class Book {
+	protected String id;
+	protected float price;
+	protected String publisher;
+	
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(String id, float price, String publisher) {
+		this.id = id;
+		this.price = price;
+		this.publisher = publisher;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	
+}
