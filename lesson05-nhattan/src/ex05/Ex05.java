62e15ba308ddbed3783d580152209c92d5d185b3
My <myle10032001@gmail.com>
2021-10-09 17:46:05 +0700

    lesson06 9.10.2021 remake

--- lesson05-nhattan/src/ex05/Ex05.java
+++
@@ -1,7 +0,0 @@
-package ex05;
-
-public class Ex05 {
-	public static void main(String[] args) {
-		
-	}
-}
--- lesson05-nhattan/src/ex05/TextBook.java
+++
@@ -1,6 +0,0 @@
-package ex05;
-
-public class TextBook extends Book{
-	private boolean isNew;
-	
-}
