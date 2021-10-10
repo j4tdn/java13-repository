7fcaba48f8cc71ecb85f2bee9a8cc8fc76025b8d
NguyenThiTuAnh <Nguyenthituanh0507@gmail.com>
2021-10-10 10:47:15 +0700

    lesson06-exception 10.10.2021

---
+++ lesson06-exception/src/exception/Ex00.java
@@ -0,0 +1,21 @@
+package exception;
+/**
+ * Enter a year of birth >> calculate age of candidate
+ */
+
+import java.time.Year;
+import java.util.Scanner;
+
+public class Ex00 {
+	
+	private static Scanner ip = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("Enter year of birth: ");
+		int yob = Integer.parseInt(ip.nextLine());
+		int cry = Year.now().getValue();
+		
+		System.out.println("Age: " + (cry - yob) );
+	}
+
+}
