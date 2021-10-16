6aa9af57a5ebc1810648cad00f73cfd77be4f346
tanbeo <nhattandoan273@gmail.com>
2021-10-16 16:20:12 +0700

    lesson06-exception 16.10.2021

--- lesson06-exception/readme.md
+++ lesson06-exception/readme.md
@@ -1,10 +1,16 @@
 Runtime, Unchecked - Exception
 Compile, Checked - Exception
 
+TODO : SelfException
+
 --Solve
 1. Validation >> Combination
 2. Try Catch >> everywhere
 3. Throw, Throws >> error first >> funtion
+
+Keyword: finally >> always run with catch exception or not
+
+==========================================================
 
 Lesson06 - Exception
 Exception 01 ...
---
+++ lesson06-exception/src/exception/Ex06.java
@@ -0,0 +1,27 @@
+package exception;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class Ex06 {
+	private static Random rd = new Random();
+	
+	public static void main(String[] args) {
+		// name, age >> required: age >= 18
+		// 16 - 20
+		int age = 16 + rd.nextInt(5);
+		String ticket = bookTicket("Tani", 16 + rd.nextInt(5));
+		System.out.println(ticket);
+	}
+	
+	private static String bookTicket(String name, int age) {
+		if(name == null) {
+			throw new NullPointerException("Name cannot be null");
+		}
+		Objects.requireNonNull(name, "Name cannot be null");
+		if(age < 18) {
+			throw new RuntimeException("Invalid Age (>= 18)");
+		}
+		return "tk123";
+	}
+}
