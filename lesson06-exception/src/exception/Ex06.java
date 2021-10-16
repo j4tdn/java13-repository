0aed887443d4e7525e68427951f9a19db6bf8e10
NguyenThiTuAnh <Nguyenthituanh0507@gmail.com>
2021-10-16 16:18:08 +0700

    lesson06-exception 16.10.2021

---
+++ lesson06-exception/src/exception/Ex06.java
@@ -0,0 +1,29 @@
+package exception;
+
+import java.util.Objects;
+import java.util.Random;
+
+import self.InvalidAgeException;
+
+public class Ex06 {
+	
+	private static Random rd = new Random();
+	public static void main(String[] args) {
+		// Name, age >> required: age >= 18
+		try {
+			String ticket = bookTicket(null, 16 + rd.nextInt(5));
+			System.out.println(ticket);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static String bookTicket(String name, int age) throws Exception {
+		Objects.requireNonNull(name, "Name cannot be null");
+		if(age < 18) {
+			throw new InvalidAgeException("Invalid Age (>=18)");
+		}
+		return "tk123";
+	}
+	
+}
