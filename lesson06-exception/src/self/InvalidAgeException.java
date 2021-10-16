bf22f6bd349e3bf46f8ca23e599364943ae04e80
VietDev <102190298@sv1.dut.udn.vn>
2021-10-16 16:58:52 +0700

    lesson06-exception 16.10.2021

---
+++ lesson06-exception/src/self/InvalidAgeException.java
@@ -0,0 +1,12 @@
+package self;
+
+public class InvalidAgeException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
