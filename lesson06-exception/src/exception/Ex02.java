d7a42703c287e862819f96fd9654600bf49e585f
minh1902 <dovanmin1902@gmail.com>
2021-10-16 11:45:21 +0700

    lesson06-exception 16.10.2021

--- lesson06-exception/src/exception/Ex02.java
+++ lesson06-exception/src/exception/Ex02.java
@@ -4,20 +4,29 @@ import java.time.Year;
 import java.util.Scanner;
 
 public class Ex02 {
-	
-	private static Scanner ip = new Scanner(System.in); 
-	
+
+	private static Scanner ip = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		System.out.println("Enter year of birth: ");
+		
+
 		int yob = 0;
-		try {
-		yob = Integer.parseInt(ip.nextLine());
-		int cry = Year.now().getValue();
-		System.out.println("Age: " + (cry - yob));
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
+
+		while(true) {
+			try {
+				//block scope
+				//NumberFormatException
+				System.out.println("Enter Year Of Birth: ");
+				yob = Integer.parseInt(ip.nextLine());
+				
+				int nowYear = Year.now().getValue();
+				System.out.println("Age: " + (nowYear - yob)); 
+				break;
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
 		}
 		System.out.println("Back to Main method");
-	} 
-	
+
+	}
 }
---
+++ lesson06-exception/src/exception/Ex04.java
@@ -0,0 +1,18 @@
+package exception;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		int [] a = {1,2,3};
+		test(a);
+		
+		
+	}
+	private static void test(int [] a) {
+		// Null Pointer Exception
+		a[1] =0;
+		a[2] = 1;
+		
+		// ArrayIndexOutOfBoudary  
+		a[3] = 20;
+	}
+}
