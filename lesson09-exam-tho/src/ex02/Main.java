6beec028725542749f75fefde4a8b6cc400f3abe
MGM\tcho <trung.cong.ho@mgm-tp.com>
2021-11-13 17:39:23 +0700

    lesson09-trungho

--- lesson09-exam-tho/src/ex02/Main.java
+++ lesson09-exam-tho/src/ex02/Main.java
@@ -4,16 +4,26 @@ public class Main {
 	public static void main(String[] args) {
 		String s1 = "01a2b3456cde478";
 		
-		getLargestNumbers(s1);
-		
+		// Get max number from string
+		System.out.println("Max number from string: " + getLargestNumbers(s1));
 	}
 	
-	public static String getLargestNumbers(String string) {
+	public static int getLargestNumbers(String string) {
 		String[] separateDigit = string.split("[a-z]+");
 		
+		Integer[] number = new Integer[separateDigit.length];
+		
 		for(int i = 0; i < separateDigit.length; i++) {
-			System.out.println(separateDigit[i]);
+			number[i] = Integer.parseInt(separateDigit[i]);
 		}
-		return string;
+		
+		int max = number[0];
+		for(int i = 0; i < number.length; i++) {
+			if (max < number[i]) {
+				max = number[i];
+			}
+		}
+		
+		return max;
 	}
 }
