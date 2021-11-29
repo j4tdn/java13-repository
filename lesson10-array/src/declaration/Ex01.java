545b7afe02527ced695dcabc8c6536a9705071f5
NguyenThiTuAnh <Nguyenthituanh0507@gmail.com>
2021-11-29 19:52:09 +0700

    lesson10-array 29.11.2021

---
+++ lesson10-array/src/declaration/Ex01.java
@@ -0,0 +1,34 @@
+package declaration;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import bean.Item;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		// initial arrays with size
+				int[] digits = new int[6];
+				digits[1] = 2;
+				modify(digits);
+				System.out.println("digits: " + Arrays.toString(digits));
+				System.out.println("\n========\\\\\\\\=========\n");
+
+				String[] sequences = new String[4];
+				sequences[0] = "Empty";
+				System.out.println("sequences: " + Arrays.toString(sequences));
+				System.out.println("\n========\\\\\\\\=========\n");
+
+				boolean startWithSGK = (sequences[1] != null && sequences[1].startsWith("SGK"));
+				System.out.println("Start with SGK: " + startWithSGK);
+				System.out.println("\n========\\\\\\\\=========\n");
+
+				Item[] items = new Item[8];
+				items[0] = new Item(123, "Item 123", 220, LocalDate.now());
+				System.out.println("items: " + Arrays.toString(items));
+	}
+	
+	private static void modify(int[] input) {
+		input[2] = 1;
+	}
+}
