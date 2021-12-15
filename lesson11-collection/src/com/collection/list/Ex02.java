7d1a71965ed340c573f332c868bf1e64e230cd65
ThanhNguyen1392001 <nguyen13701@gmail.com>
2021-12-15 22:21:48 +0700

    lesson11-collection 15.12.2021

---
+++ lesson11-collection/src/com/collection/list/Ex02.java
@@ -0,0 +1,50 @@
+
+package com.collection.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * // heap: normal || constant pool
+ * List ArrayList with Single Object Type – Integer, String
+ * 
+ * JDK 1.5 1.7
+ * 
+ * JDK 1.5
+ * Boxing, UnBoxing, AutoBoxing
+ * int - Integer
+ * float - Float
+ * double - Double
+ * long - Long
+**/
+
+public class Ex02 {
+	public static void main(String[] args) {
+		// Required: List<Integer> List<String>
+		
+		List<Integer> digits = new ArrayList<>();
+		
+		digits.add(14);
+		digits.add(22);
+		digits.add(36);
+		
+		// digits.add("10 triệu"); mandatory is Integer
+		
+		// error at compile time
+		
+		for (Integer digit: digits) {
+			System.out.println(digit * 2);
+		}
+		
+		System.out.println("\\\\\\\\\\==============\\\\\\\\\\");
+		
+		List<String> students = new ArrayList<>();
+		students.add("John smith");
+		students.add("Array peter");
+		
+		for (String student: students) {
+			System.out.println(student);
+		}
+		
+	}
+}
