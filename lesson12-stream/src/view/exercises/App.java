5a504b6b4c090ce77157ba97e552e89792636742
VietDev <102190298@sv1.dut.udn.vn>
2022-03-02 21:09:29 +0700

    lesson12-stream 02.03.2022

--- lesson12-stream/src/view/exercises/App.java
+++ lesson12-stream/src/view/exercises/App.java
@@ -24,14 +24,12 @@ public class App {
 	public static void main(String[] args) {
 		List<Transaction> transactions = DataModel.getTransactions();
 		List<Trader> traders = DataModel.getTraders();
-		// 1. Find all transactions in the year 2011 and sort them by value (small to
-		// high).
+		// 1. Find all transactions in the year 2011 and sort them by value (small to high).
 		List<Transaction> rq1 = transactions.stream().filter(t -> t.getYear() == 2011)
-				.sorted(Comparator.comparing(Transaction::getYear)).collect(Collectors.toList());
+				.sorted(Comparator.comparing(Transaction::getValue)).collect(Collectors.toList());
 		log(rq1);
 
-		// 2. Find all transactions have value greater than 300 and sort them by
-		// trader’s city
+		// 2. Find all transactions have value greater than 300 and sort them by trader’s city
 		List<Transaction> rq2 = transactions.stream().filter(t -> t.getValue() > 300)
 				.sorted(Comparator.comparing(t -> t.getTrader().getCity())).collect(Collectors.toList());
 		log(rq2);
@@ -50,9 +48,10 @@ public class App {
 				.collect(Collectors.joining(", "));
 		System.out.println("name: " + name);
 		System.out.println("===================================");
-		// 6. Are any traders based in Milan?
+		
+		// 6. Are 0.any traders based in Milan?
 		System.out.println(
-				"Are any traders based in Milan? : " + traders.stream().allMatch(t -> "Milan".equals(t.getCity())));
+				"Are any traders based in Milan? : " + traders.stream().anyMatch(t -> "Milan".equals(t.getCity())));
 
 		// 7. Count the number of traders in Milan.
 		long count = traders.stream().filter(t -> "Milan".equals(t.getCity())).count();
@@ -63,15 +62,22 @@ public class App {
 		List<Integer> values = transactions.stream().filter(t -> "Cambridge".equals(t.getTrader().getCity()))
 				.map(Transaction::getValue).collect(Collectors.toList());
 		log(values);
-		// 9. What’s the highest value of all the transactions?
-		Transaction highestValue = transactions.stream().reduce(transactions.get(0),
-				(t1, t2) -> t1.getValue() > t2.getValue() ? t1 : t2);
-		System.out.println("highest value of all the transactions: " + highestValue.getValue());
+		
+		// 9. What’s the highest value of all the transactions? (REDUCE)
+//		int highestValue = transactions.stream().reduce(transactions.get(0), (t1, t2) -> t1.getValue() > t2.getValue() ? t1 : t2).getValue();
+		int highestValue = transactions.stream().map(Transaction::getValue).reduce(Integer.MIN_VALUE, Math::max);
+		System.out.println("highest value of all the transactions: " + highestValue);
+		
 		// 10. Find the transaction with the smallest value.
-		Transaction smallestValue = transactions.stream().reduce(transactions.get(0),
-				(t1, t2) -> t1.getValue() < t2.getValue() ? t1 : t2);
-		System.out.println("===================================");
-		System.out.println("smallest value of all the transactions: " + smallestValue.getValue());
+		int smallestValue = transactions.stream().map(Transaction::getValue).reduce(Integer.MIN_VALUE, Math::min);
+		System.out.println("smallest value of all the transactions: " + smallestValue);
+
+		
+		
+// 		Transaction::getTrader <=> Function<Transaction, Trader>
+//		Function<Transaction, Trader> f0 = Transaction::getTrader;
+//		Function<Trader, String> f1 = Trader::getCity;
+//		Function<Transaction, String> fn = f0.andThen(f1);
 	}
 
 	public static <T> void log(List<T> things) {
