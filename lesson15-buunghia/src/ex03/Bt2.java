c571fd42e6069c228a6b2e0676a1f10144ffaab4
unknown <totapchoi@gmail.com>
2022-03-19 17:15:50 +0700

    19/03/2022

---
+++ lesson15-buunghia/src/ex03/Bt2.java
@@ -0,0 +1,17 @@
+package ex03;
+import java.util.Map;
+import java.util.HashMap;
+public class Bt2 {
+	public static void main(String[] args) {
+		String str= "abddeff";
+		Map<String,Integer> map = new HashMap<>();
+		for (int i=0 ; i < str.length(); i++) {
+			
+
+		}
+			
+				
+		
+		
+	}
+}
