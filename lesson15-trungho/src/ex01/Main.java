cbdd669e02a382e808a2667f5a6f9085bf6fa2e4
MGM\tcho <trung.cong.ho@mgm-tp.com>
2022-03-19 17:21:33 +0700

    lesson15-trungho

---
+++ lesson15-trungho/src/ex01/Main.java
@@ -0,0 +1,35 @@
+package ex01;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+	public static void main(String[] args) {
+		List<Integer> numberList1 = Arrays.asList(1, 2, 3, 4, 5, 6, 5, 5, 3, 1);
+		
+		System.out.println("Numbers display 1 time in the input list:");
+		List<Integer> resultList1 = numberList1.stream()
+			   .collect(Collectors.groupingBy(number -> number, Collectors.counting()))
+			   .entrySet()
+		       .stream()
+		       .filter(entry -> entry.getValue() == 1)
+		       .map(entry -> entry.getKey())
+		       .collect(Collectors.toList());
+		resultList1.forEach(System.out::println);
+		
+		System.out.println("------------------------------------------------");
+		
+		List<Integer> numberList2 = Arrays.asList(5, 7, 9, 10, 20, 9, 7, 11);
+		
+		System.out.println("Numbers display more than 1 time in the input list:");
+		List<Integer> resultList2 = numberList2.stream()
+			   .collect(Collectors.groupingBy(number -> number, Collectors.counting()))
+			   .entrySet()
+		       .stream()
+		       .filter(entry -> entry.getValue() > 1)
+		       .map(entry -> entry.getKey())
+		       .collect(Collectors.toList());
+		resultList2.forEach(System.out::println);
+	}
+}
---
+++ lesson15-trungho/src/ex03/Main.java
@@ -0,0 +1,58 @@
+package ex03;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		String str1 = " Wor  d";
+		String str2 = "  or Wd ";
+
+		String str3 = "Hello";
+		String str4 = "Hellloo";
+
+		System.out.println("Is 'String 1' and 'String 2' anagram? ---> " + anagram(str1, str2));
+		System.out.println("Is 'String 3' and 'String 4' anagram? ---> " + anagram(str3, str4));
+
+		System.out.println("------------------------------------------------");
+		String inputString = "aaaccdcee";
+
+		System.out.println("Character with highest frequency in the string: "
+				+ getCharacterWithMaxFrequency(splitSameStringToArray(inputString)));
+	}
+
+	private static boolean anagram(String str1, String str2) {
+		String str1AfterProcess = sortInline(str1.replaceAll("\\s+", "").toLowerCase());
+		String str2AfterProcess = sortInline(str2.replaceAll("\\s+", "").toLowerCase());
+
+		if (str1AfterProcess.length() != str2AfterProcess.length()) {
+			return false;
+		}
+
+		return (str1AfterProcess.equals(str2AfterProcess));
+	}
+
+	public static String sortInline(String input) {
+		char[] temps = input.toCharArray();
+		Arrays.sort(temps);
+		return String.valueOf(temps);
+	}
+
+	public static String[] splitSameStringToArray(String input) {
+		String s1 = sortInline(input);
+		String[] out = s1.split("(?<=(.))(?!\\1)");
+		Arrays.sort(out);
+		return out;
+	}
+
+	public static char getCharacterWithMaxFrequency(String[] stringArray) {
+		int max = 0;
+		int index = 0;
+		for (int i = 0; i < stringArray.length; i++) {
+			if (max < stringArray[i].length()) {
+				max = stringArray[i].length();
+				index = i;
+			}
+		}
+		return stringArray[index].charAt(0);
+	}
+}
