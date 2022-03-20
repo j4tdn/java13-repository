51f479ef90467900ecd1d67d3d52e08fbf417297
NguyenThiTuAnh <Nguyenthituanh0507@gmail.com>
2022-03-20 22:35:43 +0700

    lesson15-tuanh 20/03/2022

---
+++ lesson15-tuanh/src/ex03/Ex3_2.java
@@ -0,0 +1,30 @@
+package ex03;
+
+public class Ex3_2 {
+	public static void main(String[] args)
+	{
+		String str = "aaaababbbddc";
+		System.out.println("String is: "+ str);
+		characterCount(str);
+	}
+	public static void characterCount(String str)
+	{
+		int counter[] = new int[256];
+		int len = str.length();
+		for (int i = 0; i < len; i++)
+			counter[str.charAt(i)]++;
+		char array[] = new char[str.length()];
+		for (int i = 0; i < len; i++) {
+			array[i] = str.charAt(i);
+			int flag = 0;
+			for (int j = 0; j <= i; j++) {
+				if (str.charAt(i) == array[j])
+					flag++;
+			}
+			if (flag == 1)
+				System.out.println("The number of occurrences of " + str.charAt(i)
+				+ " in the string is: " + counter[str.charAt(i)]);
+		}
+	}
+
+}
