4ab1d92a5ac0cd48dcf73fec71a0d22fc6f2da5e
Quyen Phan <Quyen.Phan@mgm-tp.com>
2022-03-23 19:57:44 +0700

    Review - lesson15-vuvietanh

--- lesson15-vuvietanh/src/Ex01/App.java
+++ lesson15-vuvietanh/src/Ex01/App.java
@@ -1,3 +1,5 @@
+// Tên package convention thường sẽ viết thường 100%
+// Tên class theo camel case và chữ đầu tiên viết hoa
 package Ex01;
 
 import java.util.Arrays;
@@ -7,10 +9,16 @@ import java.util.stream.Collectors;
 public class App {
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList( 2, 4, 3, 5, 7, 12, 2, 3, 5, 9 );
+		// A VA hiểu sai đề câu 1. Xuất hiện 1 lần khác với distinct nha
+		// VD: 1 1 2 3
+		// distinct: 1 2 3
+		// one-time: 2 3
 		List<Integer> result = numbers.stream().distinct().collect(Collectors.toList());
 		System.out.println(result);
 		System.out.println("====================");
 		
+		// good solution
+		// câu trên a chỉ cần làm như này và điều kiện chỗ filter == 1 là xong
 		List<Integer> uniqueElements = numbers.stream()
 				   .collect(Collectors.groupingBy(number -> number, Collectors.counting()))
 				   .entrySet()
--- lesson15-vuvietanh/src/Ex03/App.java
+++ lesson15-vuvietanh/src/Ex03/App.java
@@ -1,52 +1,53 @@
 package Ex03;
 
-import java.util.Scanner;
-
 public class App {
 	public static void main(String[] args) {
-		boolean result =  isAnagram("Word", "droW");
+		boolean result = isAnagram("Word", "odrW");
 		System.out.println(result);
-		
-		System.out.println("=======================");
-		
-		char c = counting("aaavvvjjjj");
-		System.out.println(c);
-	}	
-		private static char counting(String s) {
-			
-			int count[] = new int[256];
-		      
-	        int len = s.length();
-	        for (int i=0; i<len; i++)
-	            count[s.charAt(i)]++;
-	      
-	        int max = Integer.MIN_VALUE;
-	        char result = ' ';
 
-	        for (int i = 0; i < len; i++) {
-	            if (max < count[s.charAt(i)]) {
-	                max = count[s.charAt(i)];
-	                result = s.charAt(i);
-	            }
-	        }
-	      
-	        return result;
-		
+		System.out.println("=======================");
+
+		// và trường hợp có thể số lượng ký tự bằng nhau như aaaavvvv đều có 4 ký tự a chưa xử lý
+		char c = counting("aaaavvvjjjj");
+		System.out.println(c);
 	}
-	 public static boolean isAnagram(String s1, String s2) {
-	        if (s1 == null || s2 == null) {
-	            return false;
-	        }
-	        if (s1.length() != s2.length()) {
-	            return false;
-	        }
-	        int len = s1.length();
-	        for (int i = 0; i < len; i++) {
-	            if (s1.charAt(i) != s2.charAt(len - 1 - i)){
-	                return false;
-	            }
-	        }
-	        return true;
-	    }
+
+	private static char counting(String s) {
+
+		int count[] = new int[256];
+
+		int len = s.length();
+		for (int i = 0; i < len; i++)
+			count[s.charAt(i)]++;
+
+		int max = Integer.MIN_VALUE;
+		char result = ' ';
+
+		for (int i = 0; i < len; i++) {
+			if (max < count[s.charAt(i)]) {
+				max = count[s.charAt(i)];
+				result = s.charAt(i);
+			}
+		}
+
+		return result;
+
+	}
+
+	public static boolean isAnagram(String s1, String s2) {
+		if (s1 == null || s2 == null || s1.length() != s2.length()) {
+			return false;
+		}
+
+		// Solution này a dùng để kiểm tra 2 chuỗi có đối xứng không
+		// Còn để bài là anagram, là cùng số lượng ký tự còn xuất hiện ở đâu cũng được cả
+		int len = s1.length();
+		for (int i = 0; i < len; i++) {
+			if (s1.charAt(i) != s2.charAt(len - 1 - i)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
